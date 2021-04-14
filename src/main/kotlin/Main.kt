b92eb740d175e20c8513ffe9645988a97b403ab7
ZerdaRemora <james@zerdaremora.tech>
2021-04-14 23:36:53 +0100

    Added drop benefit query and database tables.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,12 +12,25 @@ import org.slf4j.LoggerFactory
 import java.time.Instant
 import java.time.ZoneOffset
 import java.util.*
+import kotlin.collections.HashMap
 
+// Game data class
 data class Game(val id: Int, val displayName: String, val boxArtURL: String)
+
+// Drop Campaign data classes
 data class DropCampaign(val id: String, val name: String, val game: Game, val status: String, val startAt: Date, val endAt: Date)
 data class CurrentUser(val login: String, val dropCampaigns: List<DropCampaign>)
 data class ResponseData(val currentUser: CurrentUser)
 data class ApiResponse(val data: ResponseData)
+
+// Campaign Details data classes
+data class Benefit(val id: String, val name: String, val imageAssetURL: String)
+data class BenefitEdges(val benefit: Benefit, val entitlementLimit: Int)
+data class TimeBasedDrops(val id: String, val name: String, val requiredMinutesWatched: Int, val preconditionDrops: List<TimeBasedDrops>?, val benefitEdges: List<BenefitEdges>)
+data class DropCampaignDetails(val id: String, val timeBasedDrops: List<TimeBasedDrops>)
+data class DetailsUser(val dropCampaign: DropCampaignDetails)
+data class DetailsResponseData(val user: DetailsUser)
+data class DetailsApiResponse(val data: DetailsResponseData)
 
 object Games: Table() {
     val id = integer("id").uniqueIndex()
@@ -38,6 +51,23 @@ object Drops: Table() {
     override val primaryKey = PrimaryKey(id, name = "PK_Drops_ID")
 }
 
+object DropBenefits: Table() {
+    val id = varchar("id", 255).uniqueIndex()
+    val name = varchar("name", 255)
+    val dropId = reference("drop_id", Drops.id)
+    val requiredMinutesWatched = integer("required_minutes_watched")
+    val entitlementLimit = integer("entitlement_limit")
+    val benefitArtUrl = varchar("benefit_art_url", 255).nullable()
+
+    override val primaryKey = PrimaryKey(id, name = "PK_DropBenefits_ID")
+}
+
+object DropBenefitPreReqs: Table() {
+    val benefitId = reference("benefit_id", DropBenefits.id)
+    val preReqBenefitId = reference("pre_req_benefit_id", DropBenefits.id)
+
+    override val primaryKey = PrimaryKey(benefitId, preReqBenefitId, name = "PK_DropBenefitPreReqs_ID")
+}
 
 fun main() {
     val logger = LoggerFactory.getLogger("twitch-drop-campaigns")
@@ -50,6 +80,7 @@ fun main() {
     val pgPassword = dotenv["POSTGRES_PASSWORD"] ?: "postgres"
     val oauth = dotenv["OAUTH_TOKEN"]
     val clientId = dotenv["CLIENT_ID"]
+    val channelLogin = dotenv["CHANNEL_LOGIN"]
 
     val requestBody = "[{\"operationName\":\"ViewerDropsDashboard\",\"variables\":{},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"c4d61d7b71d03b324914d3cf8ca0bc23fe25dacf54120cc954321b9704a3f4e2\"}}}]"
 
@@ -58,8 +89,9 @@ fun main() {
         .header(Headers.AUTHORIZATION, oauth) // TODO: This can and probably will change.
         .header("Client-ID", clientId)
         .responseObject<List<ApiResponse>>()
-
     Database.connect(buildConnectionPool(pgHost, pgPort, pgPassword))
+
+    val allDropIds = mutableListOf<String>()
 
     transaction {
         SchemaUtils.createMissingTablesAndColumns(Games)
@@ -73,6 +105,16 @@ fun main() {
                 col[boxArtUrl] = it.game.boxArtURL
             }
 
+            /*
+            // Fix box art urls:
+            Games.update({ Games.id eq it.game.id}) { update ->
+                update[boxArtUrl] = it.game.boxArtURL
+            }
+            */
+
+            if (it.status != "EXPIRED")
+                allDropIds.add(it.id)
+
             val campaignCount = Drops.select { Drops.id eq it.id }.count()
             if (campaignCount == 0L) {
                 alertNewCampaign(it, logger)
@@ -81,7 +123,7 @@ fun main() {
                 Drops.select { Drops.id.eq(it.id) and Drops.status.neq(it.status) }.forEach { drop ->
                     logger.info("[Update] ${drop[Drops.name]} (${drop[Drops.started]}) status changed to ${it.status}")
 
-                    Drops.update({ Drops.id eq it.id}) { update ->
+                    Drops.update({ Drops.id eq it.id }) { update ->
                         update[status] = it.status
                     }
                 }
@@ -98,6 +140,70 @@ fun main() {
             }
         }
     }
+
+    transaction {
+        SchemaUtils.createMissingTablesAndColumns(DropBenefits)
+        SchemaUtils.createMissingTablesAndColumns(DropBenefitPreReqs)
+
+        val chunkedDropsList = allDropIds.chunked(35) // API has a limit of 35 campaigns per request
+
+        chunkedDropsList.forEach { outer ->
+            val sj = StringJoiner(",", "[", "]")
+            outer.forEach {
+                val detailsRequestBody =
+                    "{\"operationName\":\"DropCampaignDetails\",\"variables\":{\"dropID\": \"${it}\", \"channelLogin\": \"${channelLogin}\"},\"extensions\":{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"14b5e8a50777165cfc3971e1d93b4758613fe1c817d5542c398dce70b7a45c05\"}}}"
+                sj.add(detailsRequestBody)
+            }
+
+            val campaignDetails = getCampaignDetails(sj.toString(), oauth, clientId)
+            campaignDetails.forEach {
+                val campaign = it.data.user.dropCampaign
+                val preReqMap = HashMap<String, MutableList<String>>().withDefault { mutableListOf() }
+
+                campaign.timeBasedDrops.forEach { dropItem ->
+                    val benefit = dropItem.benefitEdges[0] // Have not yet seen a TimeBasedDrops entity with more than one benefit edge.
+                    DropBenefits.insertIgnore { col ->
+                        col[id] = dropItem.id
+                        col[name] = benefit.benefit.name
+                        col[dropId] = campaign.id
+                        col[requiredMinutesWatched] = dropItem.requiredMinutesWatched
+                        col[entitlementLimit] = benefit.entitlementLimit
+                        col[benefitArtUrl] = benefit.benefit.imageAssetURL
+                    }
+
+                    if (dropItem.preconditionDrops != null) {
+                        dropItem.preconditionDrops.forEach { preReq ->
+                            if (preReqMap[dropItem.id].isNullOrEmpty()) {
+                                preReqMap[dropItem.id] = mutableListOf(preReq.id)
+                            } else {
+                                preReqMap[dropItem.id]?.add(preReq.id)
+                            }
+                        }
+                    }
+                }
+
+                preReqMap.forEach { (drop, preReqDrops) ->
+                    preReqDrops.forEach { preReq ->
+                        DropBenefitPreReqs.insertIgnore { col ->
+                            col[benefitId] = drop
+                            col[preReqBenefitId] = preReq
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+fun getCampaignDetails(requestBody: String, oauth: String, clientId: String): List<DetailsApiResponse> {
+    val (_, _, result) = "https://gql.twitch.tv/gql".httpPost()
+        .body(requestBody)
+        .header(Headers.AUTHORIZATION, oauth)
+        .header("Client-ID", clientId)
+        .responseObject<List<DetailsApiResponse>>()
+
+    return result.get()
 }
 
 fun buildConnectionPool(pgHost: String, pgPort: String, pgPassword: String): HikariDataSource {
